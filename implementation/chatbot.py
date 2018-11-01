76962c2e9bd538f6186c9fa8c550558f901a949a
Tim Urista <timothy.urista@gmail.com>
2018-11-01 07:00:15 -0700

    add final step to building brain on chatbot

--- implementation/chatbot.py
+++ implementation/chatbot.py
@@ -368,4 +368,56 @@ def decoder_rnn(decoder_embedded_input,
                     )
     return training_predictions, test_predictions
         
-        
+
+## Building seq2seq model
+## targets = answers
+def seq2seq_model(inputs, 
+                  targets, 
+                  keep_prob, 
+                  batch_size, 
+                  sequence_length, 
+                  answers_num_words,
+                  questions_num_words,
+                  # number dimensions for encoder/decoder
+                  encoder_embedding_size,
+                  decoder_embedding_size,
+                  rnn_size,
+                  num_layers,
+                  questionswords2int):
+    ## return training and test predictions
+    ## assemblage, return everything
+    encoder_embedded_input = tf.contrib.layers.embed_sequence(inputs,
+                                                              answers_num_words + 1,
+                                                              encoder_embedding_size,
+                                                              initializer = tf.random_uniform_initializer(0, 1))
+    ## feed rnn
+    encoder_state = encoder_rnn_layer(encoder_embedded_input, 
+                                      rnn_size, 
+                                      num_layers, 
+                                      keep_prob,
+                                      sequence_length)
+    ## get preprocessed stuff
+    ## to backpropogate the loss
+    preprocessed_targets = preprocess_targets(targets, questionswords2int, batch_size)
+    
+    # matrix
+    decoder_embeddings_matrix = tf.Variable(tf.random_uniform([questions_num_words + 1, decoder_embedding_size], 0, 1))
+
+    # use matrix to get decoder embedded inputs
+    decoder_embedded_input = tf.nn.embedding_lookup(decoder_embeddings_matrix, preprocessed_targets)
+    
+    ## now we can decode things
+    training_predictions, test_predictions = decoder_rnn(decoder_embedded_input,
+                                                         decoder_embeddings_matrix,
+                                                         encoder_state,
+                                                         questions_num_words,
+                                                         sequence_length,
+                                                         rnn_size,
+                                                         num_layers,
+                                                         questionswords2int,
+                                                         keep_prob,
+                                                         batch_size)
+    
+    return training_predictions, test_predictions
+    
+    
---
+++ implementation/readme.md
@@ -0,0 +1,2 @@
+## Examples found here
+https://www.superdatascience.com/deep-learning-chatbot/
